c213718aae878dba6855eef890ad3ebe527fbfbf
Ryan Haney <yoshiquest@gmail.com>
2015-04-23 14:50:16 -0500

    Updated to Version 0.1.0

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # Translation Overlay
 
-An overlay for translating (or "subtitling") games. Currently very in-dev.
+An overlay for translating (or "subtitling") games. Currently in-dev, and works for the first game in the Lenen Project: Evanescent Existence.
 
 ## Usage
 
-If you want to compile the source code, install leiningen and use that on the project.
+To use the program, run this and the game in question (as of now, Lenen Project: Evanescent Existence). On windows, it should be good to go immediately. On mac the game will become unfocused when the game starts, so use mission control (swipe up with three fingers) to put the window back into focus, which should make it work so long as you don't click on the game window.
 
-Note that it currently does not work on OSX, and is untested for Linux. The differences in handling windows are making it a pain to work with.
+This program can't pick up what goes on in game, so the user needs to hit certain keys in order to control the overlay. By default, these keys are:
+
+c key: shows/hides the dialogue box. Press this upon reaching a boss or another place with dialogue.
+
+z key: displays the next line in the dialogue. Overwrites the normal dialogue advancing key, so that the game's dialogue moves as well as the overlay's dialogue. Only works when dialogue is visible.
+
+a key: moves dialogue back one stage without affecting the game. Only works when dialogue is visible.
+
+s key: moves the dialogue forward one stage without affecting the game. Only works when dialogue is visible.
+
+Currently only supports Yabusame's main scenario. Other scenarios will be supported in time. Also only supports windowed mode. Fullscreen mode will also hopefully be supported in time. Windows should not be moved from their default positions, as the overlay does not move yet.
+
+I have many plans for this overlay, which will be implemented in time!
 
 ## Changelog
+
+Version 0.1.0 - Very large update. I've added much support to the game, and added .edn files to the resources folder so that one without coding knowledge can somewhat translate (hopefully). There are still many things to do, but for now it works with Yabusame's Scenario in Evanescent Existance. Many things were added, such as keys for the dialogue box, as well as support for .edn files, which can be used to edit the translation, dialogue size and position, amoung other things. Still somewhat limited, but is presentable for now.
 
 Version 0.0.1 - Initial Commit. Currently displays the word "On!" when the 'c' key is pressed. Should even work when window is not in focus, which technically should be all the time. Using "jnativehook" for handling unfocused key presses.
 
--- src/translation_overlay/overlay.clj
+++ src/translation_overlay/overlay.clj
@@ -1,11 +1,24 @@
 (ns translation-overlay.overlay
-  (:require [translation-overlay.keys :refer [keycode]])
+  (:require [translation-overlay.keys :refer [keycode]]
+            [clojure.edn :as edn])
   (:import [java.lang Runnable]
            [java.util.logging Logger Level]
            [javax.swing JFrame JLabel SwingUtilities WindowConstants]
            [java.awt Color Toolkit Dimension]
            [org.jnativehook GlobalScreen NativeHookException SwingDispatchService]
            [org.jnativehook.keyboard NativeKeyEvent NativeKeyListener]))
+
+(def dialogue (atom []))
+(def properties (atom {}))
+
+(defn read-data [source]
+  (edn/read-string (slurp (str "resources/" source ".edn"))))
+
+(defn read-file [source destination]
+  (reset! destination (read-data source)))
+
+(read-file "dialogue" dialogue)
+(read-file "properties" properties)
 
 (defn make-runnable [func]
   (proxy [Runnable] []
@@ -23,27 +36,80 @@
     (GlobalScreen/addNativeKeyListener listener)
     listener))
 
-(defn make-label [message x y]
-  (let [label (JLabel. message)]
-    (.setLocation label x y)
-    (.setForeground label Color/GREEN)
-    (.setSize label (.getPreferredSize label))
-    label))
+(defn key->color [k]
+  (if (keyword? k)
+    (load-string (str "Color/" (name k)))
+    (load-string (str "(Color. " k ")"))))
 
-(def some-label (doto
-                  (make-label "On!" 0 0)
-                  (.setVisible false)))
+(defn make-paragraph [message]
+  (str "<html><p>" message "</p></html>"))
 
-(defn toggle-label [key-event]
-  (let [key-code-event (.getKeyCode key-event)
-        key-c (keycode :c)]
-    (if (= key-code-event key-c)
-      (.setVisible some-label (not (.isVisible some-label))))))
+(def dialogue-box (let [dialogue-properties (:dialogue @properties)]
+                    (doto
+                      (JLabel. (make-paragraph (first @dialogue)))
+                      (.setLocation (:x dialogue-properties) (:y dialogue-properties))
+                      (.setForeground (key->color (:color dialogue-properties)))
+                      (.setSize (:width dialogue-properties) (:height dialogue-properties))
+                      (.setVisible false))))
+
+(defn refresh-dialogue []
+  (let [dialogue-properties (:dialogue @properties)]
+    (doto
+      dialogue-box
+      (.setLocation (:x dialogue-properties) (:y dialogue-properties))
+      (.setSize (:width dialogue-properties) (:height dialogue-properties)))))
+
+(defn refresh-properties []
+  (read-file "properties" properties)
+  (refresh-dialogue))
+
+(defn change-dialogue-text [message]
+  (.setText dialogue-box (make-paragraph message)))
+
+(defn swap-visible []
+  (.setVisible dialogue-box (not (.isVisible dialogue-box))))
+
+(defn proceed-dialogue []
+  (if (.isVisible dialogue-box)
+    (do
+      (swap! dialogue rest)
+      (if (= (first @dialogue) :hide)
+        (do
+          (.setVisible dialogue-box false)
+          (swap! dialogue rest)))
+      (change-dialogue-text (first @dialogue)))))
+
+(defn forward []
+  (if (.isVisible dialogue-box)
+    (do
+      (swap! dialogue rest)
+      (if (not (string? (first @dialogue)))
+        (swap! dialogue rest))
+      (change-dialogue-text (first @dialogue)))))
+
+(defn backward []
+  (if (.isVisible dialogue-box)
+    (let [full-dialogue (read-data "dialogue")
+          position (- (count full-dialogue) (count @dialogue))]
+      (if (not (string? (nth full-dialogue position)))
+        (reset! dialogue (drop (- position 2) full-dialogue))
+        (reset! dialogue (drop (dec position) full-dialogue)))
+      (change-dialogue-text (first @dialogue)))))
+
+(defn on-key-pressed [key-event]
+  (let [key-code-event (.getKeyCode key-event)]
+    (condp = key-code-event
+      (keycode :c) (swap-visible)
+      (keycode :z) (proceed-dialogue)
+      (keycode :s) (forward)
+      (keycode :a) (backward)
+      (keycode :r) (refresh-properties)
+      nil)))
 
 (defn initialize-natives []
   (try
     (GlobalScreen/registerNativeHook)
-    (add-key-listener toggle-label identity identity)
+    (add-key-listener on-key-pressed identity identity)
     (catch Exception e
       (do (println (str "There was a problem registering the native hook: " (.getMessage e)))))))
 
@@ -60,5 +126,5 @@
     (.setLocation frame (- (/ (int (.getWidth screen-size)) 2) 320) (- (/ (int (.getHeight screen-size)) 2) 240))
     (.setLayout content nil)
     (.setDefaultCloseOperation frame WindowConstants/DISPOSE_ON_CLOSE)
-    (.add frame some-label)
+    (.add frame dialogue-box)
     (.setVisible frame true)))
